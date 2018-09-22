347e418a923d36a5101467b2e88d6c20f449e5a5
AbhishekChd <acechd97@gmail.com>
2018-09-22 22:56:33 +0530

    Added image loading for home screen

--- app/src/main/java/com/example/abhishek/bakeme/adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/abhishek/bakeme/adapters/RecipeAdapter.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.example.abhishek.bakeme.R;
 import com.example.abhishek.bakeme.models.Recipe;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -51,13 +52,18 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
                         .getString(R.string.people_label, mRecipes.get(i).getServings())
         );
 
+        // Load image if not an empty path
         String imagePath = mRecipes.get(i).getImage();
-
-        if (TextUtils.isEmpty(imagePath)) {
+        if (!TextUtils.isEmpty(imagePath)) {
+            Picasso.get()
+                    .load(imagePath)
+                    .placeholder(mBackgroundGradient)
+                    .error(mAppIcon)
+                    .into(recipeViewHolder.recipeImage);
+        } else {
             recipeViewHolder.recipeImage.setBackground(mBackgroundGradient);
             recipeViewHolder.recipeImage.setImageDrawable(mAppIcon);
         }
-
     }
 
     @Override
